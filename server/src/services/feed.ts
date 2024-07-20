a591acc0ed2d14c826bb8235ab0eb82053f742f2
Props <chrisding03@gmail.com>
2024-07-20 16:30:43 +0800

    fix

--- server/src/services/feed.ts
+++ server/src/services/feed.ts
@@ -29,8 +29,7 @@ export function FeedService() {
                     if (cached) {
                         return cached;
                     }
-                    let where = type === 'draft' ? eq(feeds.draft, 1) : type === 'unlisted' ? and(eq(feeds.draft, 0), eq(feeds.listed, 0)) : and(eq(feeds.draft, 0), eq(feeds.listed, 1));
-                    where = where + and(like(feeds.title, title));
+                    let where = type === 'draft' ? eq(feeds.draft, 1) : type === 'unlisted' ? and(eq(feeds.draft, 0), eq(feeds.listed, 0)) : and(eq(feeds.draft, 0), eq(feeds.listed, 1), like(feeds.title, title));
                     const size = await db.select({ count: count() }).from(feeds).where(where);
                     if (size[0].count === 0) {
                         return {
