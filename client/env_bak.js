661eae0cc65381c995bae998113b4b3b6d099d6f
Din <13716066567@163.com>
2024-08-02 16:19:21 +0800

    feat: google oauth

---
+++ client/env_bak.js
@@ -0,0 +1,38 @@
+// RIN_GITHUB_CLIENT_ID=Ov23liN3IBSd79VkzoDR
+// RIN_GITHUB_CLIENT_SECRET=3ba505516877fae6922e1f4255de912970364e93
+// JWT_SECRET=QCZiqh3u48nHIvnM5DcR
+// S3_ACCESS_KEY_ID=57f18fdf69076db7db48e4cb683a002d
+// S3_SECRET_ACCESS_KEY=55d7317a8c7181798435466f058912ee713744ee0f918a59515880ba8f421f9e
+//
+// # Use this token for authenticating against the Cloudflare API
+// R2_token_value=yU4ObOHq8faLj3JVT4A0rzf9s434uIfUlqCPul_m
+// # Use jurisdiction-specific endpoints for S3 clients
+// endpoints_default=https://e4f7d0771f5e2430f42c5842f7d260c1.r2.cloudflarestorage.com
+// endpoints_eu=https://e4f7d0771f5e2430f42c5842f7d260c1.eu.r2.cloudflarestorage.com
+//
+// # Widgets API Tokens
+// Widgets_API_Tokens=4KmJVBgJZlxqSRtAHNMTpIzq3h2u7CAnF2exJ5BI
+// Widgets_API_Tokens=tku_FC7cLWPysm0kS-E6KOn7cLicxK4mKVtEbWfW
+//
+// # Next Auth Discord Provider
+// DISCORD_CLIENT_ID=1229363438531121173
+// DISCORD_CLIENT_SECRET=89d8b226670f0682440e85e74e33853e380f0b1172b4f65ce6a1a14a3fb74c03
+//
+// # Next Auth Google provider
+// GOOGLE_CLIENT_ID=52848205396-0hie2d12onj3cikhgviu3r8idatvm3a3.apps.googleusercontent.com
+// GOOGLE_CLIENT_SECRET=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
+// GOOGLE_AUTH_CALLBACK=https://service.trendingup.top/user/google/callback
+//
+// # Notion
+// INTERNAL_INTEGRATION_SECRET=secret_WypgrpD23uzuj0ii7jKYTOjS3rx6UBW7fA7SOr91sCJ
+// CAT_DB_ID=1886682f43fc4d9c874058a5a6ba0665
+//
+// # Wechat
+// EncodingAESKey=80EcFCWUkf7VU6SH3j5gYSl2HaAysqNLaY1KFTCpOjo
+// Token=r3KG48G05nZQ8DWW
+// AppID=wx90c4f92f5024a112
+// AppSecret=1fc42d35a46046b870ea24d2d2e8b862
+//
+// # PushOver
+// PUSHOVER_USER_KEY=uochgezddzv4f9u5rj1ugdqurmowdo
+// PUSHOVER_APP_TOKEN=aerj4xa5a7vq28c4oy468vdrcypvc5
