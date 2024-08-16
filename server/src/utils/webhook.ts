edffc8b185c2871c03ae843388309373b08b4bfa
Din <13716066567@163.com>
2024-08-16 17:27:12 +0800

    feat: md

--- server/src/utils/webhook.ts
+++ server/src/utils/webhook.ts
@@ -1,17 +1,44 @@
+import {initializeApp} from "firebase/app";
+import {getMessaging} from "firebase/messaging";
+
 async function sendWebhook(url: string, data: any) {
-    return await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
+  return await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
 }
 
 export async function notify(webhook_url: string, message: string) {
-    if (!webhook_url) {
-        console.error('Please set WEBHOOK_URL')
-        return
-    }
-    return await sendWebhook(webhook_url, { content: message })
+  if (!webhook_url) {
+    console.error('Please set WEBHOOK_URL')
+    return
+  }
+  return await sendWebhook(webhook_url, {content: message})
+}
+
+export async function notify(webhook_url: string, message: string) {
+
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+  const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://DATABASE_NAME.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+    measurementId: "G-MEASUREMENT_ID",
+  };
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+  const messaging = getMessaging(app);
 }
